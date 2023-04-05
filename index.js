8eba217309883c0d7c36f9e2a5c0f86ac1845337
Veronica De Felice <verodefelic@gmail.com>
2023-04-05 14:43:17 -0400

    addede delete and update
    BUG fix update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { getAllPlants, addPlant } from "./src/plants.js"
+import { getAllPlants, addPlant, deletePlant, updatePlant } from "./src/plants.js"
 
 
 const PORT = 3001
@@ -11,8 +11,16 @@ app.use(cors())
 app.use(express.json())
 
 //api points /routes will go here..
+
+app.get("/", (req, res) => {
+    res.send(`I am root.`)
+})
+
+
 app.get("/plants", getAllPlants)
 app.post("/plants", addPlant)
+app.delete("/plants/:docId", deletePlant)
+app.patch("/plants/:docId", updatePlant)
 
 
 app.listen(PORT, () => {
